62a6ab2d41169e385dcfe1ec780c0c8e3db54520
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2023-06-12 14:45:38 +0100

    Update fahrenheitConverter.js

--- fahrenheitConverter.js
+++ fahrenheitConverter.js
@@ -6,3 +6,21 @@ let celsius = kelvin - 273;
 let fahrenheit = Math.floor((celsius * (9/5)) + 32);
 //Rounding down the fahrenheit decimal
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+
+/* ADVANCED
+// Variable for Kelvin temperature
+let kelvin = 300;
+
+//Function for the converter
+function kToFConverter(temp) {
+  // Variable for Celsius converted equivalent of Kelvin
+  let celsius = temp - 273;
+  // Variable for Fahrenheit conversion of Celsius & Rounding down the decimal
+  let fahrenheit = Math.floor((celsius * (9/5)) + 32);
+  temp = fahrenheit
+  return console.log(`The temperature is ${temp} degrees Fahrenheit.`)
+}
+
+kToFConverter(kelvin)
+*/
