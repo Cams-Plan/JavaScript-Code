6136343d5f95058414b554a69bbb5e565dfd9197
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2023-06-14 13:31:18 +0100

    Update howTo.js

--- howTo.js
+++ howTo.js
@@ -2,3 +2,16 @@
 for (let counter = 3; counter >= 0; counter--){
   console.log(counter);
 }
+
+// NESTED LOOPS
+let bobsFollowers = ['A', 'B', 'C', 'D'];
+let tinasFollowers = ['E', 'C', 'A'];
+let mutualFollowers = [];
+for (let i = 0; i < bobsFollowers.length; i++) { 
+  for (let ii = 0; ii < tinasFollowers.length; ii++) {
+    if (bobsFollowers[i] === tinasFollowers[ii]) {
+    mutualFollowers.push(tinasFollowers[ii])
+    console.log(mutualFollowers)
+    }
+  }
+}
