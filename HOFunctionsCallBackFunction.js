691c4e50c9e6abacedcdc8ffbb5420cc57667a7a
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2023-06-15 13:11:24 +0100

    Update HOFunctionsCallBackFunction.js
    added a second (more complex) example of call back functions

--- HOFunctionsCallBackFunction.js
+++ HOFunctionsCallBackFunction.js
@@ -1,4 +1,4 @@
-//CALL BACK FUNCTION EXAMPLE
+//CALL BACK FUNCTION EXAMPLE #1
 const higherOrderFunc = param => {
   param();
   return `I just invoked ${param.name} as a callback function!`
@@ -9,3 +9,25 @@ const anotherFunc = () => {
 }
  
 higherOrderFunc(anotherFunc);
+
+
+//CALL BACK FUNCTION EXAMPLE #2
+//Create inital function expression
+const addTwo = num => {
+  return num + 2;
+}
+//Create Higher Order function expression that checks the reliability of the addTwo function
+const checkConsistentOutput = (func, val) => {
+  //Produce an equation that replicates the 'addTwo' function
+  let checkA = val + 2;
+  //Call the addTwo function
+  let checkB = func(val);
+  //Validate the validity of AddTwo function by comparing the two checks and include an error message
+  if (checkA === checkB) {
+    return func(val);
+  } else {
+    return console.log('inconsistent results');
+  };
+};
+
+console.log(checkConsistentOutput(addTwo, 7));
