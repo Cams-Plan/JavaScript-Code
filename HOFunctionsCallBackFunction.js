564c0572505a4f3c40de708b03bd7731ed4b5896
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2023-06-15 12:23:09 +0100

    Create HOFunctionsCallBackFunction.js

---
+++ HOFunctionsCallBackFunction.js
@@ -0,0 +1,11 @@
+//CALL BACK FUNCTION EXAMPLE
+const higherOrderFunc = param => {
+  param();
+  return `I just invoked ${param.name} as a callback function!`
+}
+ 
+const anotherFunc = () => {
+  return 'I\'m being invoked by the higher-order function!';
+}
+ 
+higherOrderFunc(anotherFunc);
